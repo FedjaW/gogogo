7fe53ca77ac7725e94cc7cf0afabf72993605e3b
Friedrich Wilms <f.wilms89@web.de>
2024-03-17 12:53:53 +0100

    6:13 wait group

--- Cryptomaster/main.go
+++ Cryptomaster/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"leckomio.dev/go/crypto/api"
 )
 
 func main() {
-	go getCurrencyData("BTC")
-	go getCurrencyData("ETH")
-	go getCurrencyData("BCH")
+	currencies := []string{"BTC", "ETH", "BCH"}
+	var wg sync.WaitGroup
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func(currencyCode string) {
+			getCurrencyData(currencyCode)
+
+			// go supports closures
+			// hence we can access local variables that are part of the outer function
+			wg.Done()
+		}(currency) // immediately execute lambda func
+	}
+	wg.Wait()
 }
 
 func getCurrencyData(currency string) {
