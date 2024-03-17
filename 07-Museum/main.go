06ce4f7d80d54af643e8b2854dd6a02f1ecaab5f
Friedrich Wilms <f.wilms89@web.de>
2024-03-17 13:47:33 +0100

    6:48 serving a public folder

--- 07-Museum/main.go
+++ 07-Museum/main.go
@@ -5,19 +5,22 @@ import (
 	"net/http"
 )
 
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from go"))
+}
 func main() {
+	server := http.NewServeMux()
+	server.HandleFunc("/hello", handleHello) // passing a func as an argument, not executing handleHello()
 
-	http.HandleFunc("/hello",
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Hello from go"))
-		})
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs) // Handle takes a type, therefor nor HandleFunc
 
 	// will open a port in your os for incoming connections over tcp
 	// domain:port
 	// if you do not specify a host or ip address
 	// it will open that port on any network connection that you have on your computer
 	fmt.Println("start listening on port 3333")
-	err := http.ListenAndServe(":3333", nil)
+	err := http.ListenAndServe(":3333", server)
 	if err != nil {
 		fmt.Println("error while opening the server")
 	}
